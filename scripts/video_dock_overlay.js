1a4ebf7301ee089201f067deed6e0153f56d9ba2
TylerHelsel <tamhelsel@gmail.com>
2023-03-27 00:00:54 -0400

    testHTMLFind

--- scripts/video_dock_overlay.js
+++ scripts/video_dock_overlay.js
@@ -93,6 +93,7 @@ class videoOverlayData{
 Hooks.on('renderCameraViews', (playerList, html) => {
     // find the element which has our logged in user's id
     const loggedInUserListItem = html.find(`[data-user="${game.userId}"]`)
+    console.log(loggedInUserListItem)
     
     // insert a button at the end of this element
     loggedInUserListItem.append(
